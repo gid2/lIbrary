512c8efd4ce8a4425b9ee83bd25a0e3acb808e7a
gid2 <103135483+gid2@users.noreply.github.com>
2022-12-21 19:12:14 +0300

    first_version

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const config = {
     app: ['./src/components/index.jsx'],
   },
   output: {
-    path: path.resolve(__dirname, 'public'),
+    path: path.resolve(__dirname, 'public/js'),
     globalObject: 'this',
     filename: '[name].js',
   },
